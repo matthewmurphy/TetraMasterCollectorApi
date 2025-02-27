f1df182a880dcfc48d15c473d732f9e81721c84c
matthewmurphy <matthew@the-murphys.net>
2025-02-27 13:04:23 -0700

    Remove WeatherForecase sample model/controller, add Card model

---
+++ TetraMasterCollectorApi/Models/Card.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TetraMasterCollectorApi.Models
+{
+  public class Card
+  {
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    [RegularExpression("^[0-9A-F][PMXA][0-9A-F]{2}$")]
+    public required string InitialStats { get; set; }
+
+    public string[]? ObtainedFrom { get; set; }
+
+    public bool Missable { get; set; } = false;
+
+    public string? Image { get; set; }
+  }
+}
